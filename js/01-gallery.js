fa42d4a332d77d3f3c2a4c68e888d314154e8f4d
InnaShtondenko <inna.mycase@gmail.com>
2022-08-30 21:13:29 +0300

    Created script

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+let instance = null;
 
-console.log(galleryItems);
+galleryEl.addEventListener('click', onImgClick);
+const galleryMarkup = galleryItems.map(item =>
+  createGalleryItem({
+    previewImg: item.preview,
+    sizeImg: item.original,
+    description: item.description,
+  })
+);
+
+galleryEl.innerHTML = galleryMarkup.join(' ');
+
+function createGalleryItem({ previewImg, sizeImg, description }) {
+  return `<div class="gallery__item">
+            <a class="gallery__link" href=${sizeImg}>
+                <img
+                class="gallery__image"
+                src=${previewImg}
+                data-source=${sizeImg}
+                alt=${description}
+                />
+            </a>
+        </div>`;
+}
+
+function onImgClick(event) {
+  const clickTarget = event.target;
+
+  event.preventDefault();
+  if (clickTarget.nodeName != 'IMG') {
+    return;
+  }
+    
+
+const instance = basicLightbox.create(`<img
+                src=${clickTarget.dataset.source}
+                alt=${clickTarget.alt}
+                />`);
+  instance.show();
+  document.body.addEventListener('keydown', onKeyDown);
+}
+
+function onModalClose() {
+  document.body.removeEventListener('keydown', onKeyDown);
+}
+
+function onKeyDown(event) {
+  if (event.code != 'Escape') {
+    return;
+  }
+
+  instance.close(onModalClose);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
+const galleryMarkup = galleryItems.map(item =>
+  createGalleryItem({
+    previewImg: item.preview,
+    sizeImg: item.original,
+    description: item.description,
+  })
+);
+
+galleryEl.innerHTML = galleryMarkup.join(' ');
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionData: 'alt',
+});
+
+function createGalleryItem({ previewImg, sizeImg, description }) {
+  return `<a class="gallery__item" href=${sizeImg}>
+            <img class="gallery__image" src=${previewImg} alt=${description} />
+        </a>`;
+}
 console.log(galleryItems);
